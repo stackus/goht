711af21341b8a72c6d1b77a4b9516131598a76cb
Michael Stack <michael.stack@gmail.com>
2024-01-18 20:08:09 -0700

    add version to hamlet command

--- cmd/hamlet/cmd/root.go
+++ cmd/hamlet/cmd/root.go
@@ -4,21 +4,17 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+
+	"github.com/stackus/hamlet"
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "hamlet",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "A templating language for Go",
+	Long: `Hamlet is a templating language for Go. It's designed to be simple and easy to use.
+It combines Go and Haml to create a powerful templating language that's easy to learn.`,
+	Version: hamlet.Version(),
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -31,12 +27,6 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.hamlet.yaml)")
-
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
---
+++ version.go
@@ -0,0 +1,13 @@
+package hamlet
+
+import (
+	_ "embed"
+	"strings"
+)
+
+//go:embed .version
+var version string
+
+func Version() string {
+	return strings.TrimPrefix(version, "VERSION=")
+}
