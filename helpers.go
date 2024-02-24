4754b42bfe9ed1ec740385faacb8dbaaa2a363a5
Michael Stack <michael.stack@gmail.com>
2024-02-23 17:09:50 -0700

    fix the english

--- helpers.go
+++ helpers.go
@@ -1,6 +1,6 @@
 package goht
 
-// If returns trueValue if condition is true, otherwise falseValue.
+// If returns the trueValue if the condition is true, otherwise falseValue.
 func If(condition bool, trueValue, falseValue string) string {
 	if condition {
 		return trueValue
